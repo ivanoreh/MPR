ae249004807ef751f41cdb70cc96d1166a31a6a2
ivanoreh456 <ivan.oreh@hotmail.com>
2014-06-11 16:31:16 +0200

    LTS
    LTS, but yet to be cleaned for easier code reading

---
+++ src/mpr/Racun.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mpr;
+
+/**
+ *
+ * @author user
+ */
+public class Racun {
+    public String ime;
+    public String datum;
+    public String iznos;
+    public String redni_broj;
+
+    
+    
+    Racun(String im,     String dat,    String iz,     String redni ){
+        ime = im;
+        datum = dat;
+        iznos = iz;
+        redni_broj = redni;
+    }
+}
